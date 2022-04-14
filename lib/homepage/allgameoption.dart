bb293d895e2216f38cf154b9cf77254504575aba
Kamleshkkb <88033517+Kamleshkkb@users.noreply.github.com>
2022-04-14 11:56:43 +0530

    second
    All stocks and prices

--- lib/homepage/allgameoption.dart
+++ lib/homepage/allgameoption.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:stock_it_up/profilepage/userdetails.dart';
 import 'package:stock_it_up/splashscreen/login.dart';
-import 'package:http/http.dart' as http;  
+import 'package:http/http.dart' as http;
+
 class AllGameOption extends StatefulWidget {
   const AllGameOption({Key? key}) : super(key: key);
 
@@ -12,6 +15,14 @@ class AllGameOption extends StatefulWidget {
 
 class _AllGameOptionState extends State<AllGameOption> {
   FirebaseAuth auth = FirebaseAuth.instance;
+  final String apiUrl =
+      "https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&betaMoreThan=1&volumeMoreThan=10000&sector=Technology&exchange=NASDAQ&dividendMoreThan=0&limit=100&apikey=yourapikey";
+  Future<List<dynamic>> fetchStockData() async {
+    var result = await http.get(Uri.parse(apiUrl));
+    var results = json.decode(result.body);
+    return json.decode(result.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,13 +54,10 @@ class _AllGameOptionState extends State<AllGameOption> {
         child: FutureBuilder<List<dynamic>>(
           future: fetchStockData(),
           builder: (BuildContext context, AsyncSnapshot snapshot) {
-            
             if (snapshot.hasData) {
-              
               return ListView.builder(
                   padding: EdgeInsets.all(3),
-                 itemCount: snapshot.data.length,
-                 
+                  itemCount: snapshot.data.length,
                   itemBuilder: (BuildContext context, int index) {
                     return Card(
                       child: ListTile(
@@ -58,10 +66,10 @@ class _AllGameOptionState extends State<AllGameOption> {
                           children: [
                             const SizedBox(height: 9.0),
                             Text(
-                            
                               '${snapshot.data[index]['symbol']}',
-                              style: TextStyle(fontSize: 15.0,
-                              fontWeight: FontWeight.bold,
+                              style: TextStyle(
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                             const SizedBox(height: 9.0),
@@ -71,22 +79,25 @@ class _AllGameOptionState extends State<AllGameOption> {
                             ),
                           ],
                         ),
-
                         trailing: Container(
                           height: 40,
                           width: 80,
                           color: Colors.green,
-                          child: Center(child: Text("${snapshot.data[index]['price']}"
-                          ,style: TextStyle(fontSize: 20,
-                          fontWeight: FontWeight.bold),
+                          child: Center(
+                              child: Text(
+                            "${snapshot.data[index]['price']}",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
                           )),
                         ),
-                      
                       ),
                     );
                   });
             } else {
-              return Center(child: CircularProgressIndicator(color: Colors.blue,));
+              return Center(
+                  child: CircularProgressIndicator(
+                color: Colors.blue,
+              ));
             }
           },
         ),
